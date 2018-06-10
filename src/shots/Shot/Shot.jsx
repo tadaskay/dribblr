99ce95bee7dd98fbab39d39b73398339f3f09b25
Tadas Krivickas <tadas.krivickas@gmail.com>
2018-06-10 16:47:33 +0300

    Move Overlaid under common components

--- src/shots/Shot/Shot.jsx
+++ src/shots/Shot/Shot.jsx
@@ -1,7 +1,7 @@
 import React, { Fragment } from 'react';
 import PropTypes from 'prop-types';
 import './Shot.scss';
-import Overlaid from '../Overlaid/Overlaid';
+import Overlaid from '../../common/Overlaid/Overlaid';
 
 const propTypes = {
   title: PropTypes.string.isRequired,
